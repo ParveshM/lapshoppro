29d92e3213e49dad947ed452cda55173854cea6f
Parvesh M <parvesh@gmail.com>
2023-10-30 17:33:16 +0530

    Domain name is added in the query in shop

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,20 +1,17 @@
 // DataBase connnection settings---
 const mongoose = require('mongoose');
 require('dotenv').config();
+const asyncHandler = require("express-async-handler");
 
-const dbConnect = () => {
-    mongoose.connect(process.env.MONGO_URL, { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true, // Add this option
-        // Add this option
-    });
-    let db = mongoose.connection;
-    db.on('connected', () => {
-        console.log('Connected to MongoDB');
-    });
-    db.on('error', (err) => {
-        console.log(err);
-    });
-};
+
+const dbConnect = asyncHandler(async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URL);
+        console.log("Database Connected Successfully");
+    } catch (error) {
+        throw new Error();
+    }
+});
 
 module.exports = { dbConnect };
+
